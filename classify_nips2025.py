cf5a09edeeb54fc1a5c855354276e953c4adb704
Bin CHEN <setriones@gmail.com>
2025-12-16 03:01:53 +0000

    refine classify_nips2025.py log

--- classify_nips2025.py
+++ classify_nips2025.py
@@ -8,6 +8,7 @@ input_file = 'neurips2025_all_papers.json'
 logger.add("nips.log")
 
 def classify_single_paper(paper, client, selected, updated):
+    logger.info("classify paper id {}, paper title is {}, paper abstract is {}".format(paper["id"], paper["title"], paper["abstract"]))
     user_request = "Please give me a score, which is a integer between 1 and 10, indicating this paper's relevance about your research interest. The paper's tile is {}, and its abstract is {}. Please give your response in a plain text json object, the object contains 4 keys, the first is 'score', its value is the score; the second is 'Relevant Aspects', the third is 'Irrelevant Aspects' and the last is 'Summary'. Do not use any markdown gramma in your response".format(paper["title"], paper["abstract"])
     # print(user_request)
     response = client.chat.completions.create(
@@ -19,14 +20,14 @@ def classify_single_paper(paper, client, selected, updated):
     )
     response_contet = response.choices[0].message.content
     # print("paper id {}, response: {}".format(paper["id"], response_contet))
-    logger.info("classify paper id {}, response_contet is {}".format(paper["id"], response_contet))
+    logger.info("classify paper id {}, llm response_contet is {}".format(paper["id"], response_contet))
     response_obj = json.loads(response_contet)
     # print(response_obj)
     paper_cpy = copy.deepcopy(paper)
     paper_cpy['classify'] = response_obj
     updated.append(paper_cpy)
     if response_obj['score'] > 6:
-        print("find mlsys paper with id {}".format(paper["id"]))
+        logger.info("find mlsys paper with id {}".format(paper["id"]))
         paper_selected = copy.deepcopy(paper)
         paper_selected['classify'] = response_obj
         selected.append(paper_selected)
@@ -51,6 +52,7 @@ def main():
             print(sum)
             classify_single_paper(paper, client, selected, updated)
         except:
+            logger.error("classify paper {} fail".format(paper["id"]))
             continue
     json.dump(updated, updated_file)
     json.dump(selected, output_file)
